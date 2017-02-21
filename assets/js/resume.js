b55cb1d61ad48f57860e8a8a3af7865cfa1a9a22
Justin Suen <jsuen27@gmail.com>
2017-02-20 21:56:40 -0800

    Delete old things

--- assets/js/resume.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.hide td, th.sub-sect2').hide();
-    $('td.toggle, td.dum').show();
-    $('th.sub-sect, td.toggle, td.togoff').click(function() {
-       $(this).parents('table').find('td, th.sub-sect2').toggle();
-    }); 
-});
